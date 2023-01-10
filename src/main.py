a99b6ab57bf43664722fd78fc6c5f268389296d5
Barroqueiro <cruzdinis@ua.pt>
2023-01-10 16:23:26 +0000

    Working with added encryption on MAC

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-import argparse
-import hashlib
-import os
-import random
 import string
-from Cryptodome.Cipher import AES
-from Cryptodome.Protocol.KDF import PBKDF2
-from Cryptodome.PublicKey import ECC
+import argparse
 
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Random import random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 def generate_random_password():
     """Generates a random password."""
@@ -14,61 +16,62 @@ def generate_random_password():
     length = 32
 
     # Generate a random string of letters and digits
-    password = "".join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(random.sample(string.ascii_letters + string.digits, length))
 
-    return password
-
-def encrypt_password(password, public_key):
+def encrypt_with_rsa(word, public_key):
     """Encrypts a password using ECC with a public key."""
     # Encrypt the password using the public key
-    encrypted_password = public_key.encrypt(password, 32)[0]
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    return cipher_rsa.encrypt(word)
 
-    return encrypted_password
+def decrypt_with_rsa(encrypted_word, private_key):
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    return cipher_rsa.decrypt(encrypted_word)
 
 def encrypt_secure_fasta(input_file, output_file, password):
     """Encrypts a FASTA file and writes the result to a SecureFASTA file."""
     # Set the salt and number of iterations for the PBKDF2 key derivation function
-    salt = os.urandom(8)
+    salt = get_random_bytes(16)
     iterations = 10000
 
     # Use PBKDF2 to derive a key from the password
-    key = PBKDF2(password, salt, dkLen=32, count=iterations)
+    key = PBKDF2(password, salt, dkLen=32, count=iterations,hmac_hash_module=SHA256)
 
-    # Initialize the AES cipher in cipher block chaining (CBC) mode
+    # Initialize the AES cipher in cipher block chaining (OFB) mode
     cipher = AES.new(key, AES.MODE_CBC)
 
-    # Set the initialisation vector (IV) for the cipher
+    # Get the initialisation vector (IV) for the cipher
     iv = cipher.iv
 
+    # Read the DNA or protein sequence data from the input file
+    with open(input_file, "rb") as input_file:
+        # Read the data from the FASTA file
+        data = input_file.read()
+
+        # Encrypt the data using the AES cipher
+        encrypted_data = cipher.encrypt(pad(data, 16))
+
     # Open the output file for writing
-    with open(output_file, "w") as f:
+    with open(output_file, "wb") as f:
         # Write the salt, iterations, and IV to the file
         f.write(salt)
-        f.write(iterations)
+        f.write(iterations.to_bytes(8,'big'))
         f.write(iv)
 
-        # Read the DNA or protein sequence data from the input file
-        with open(input_file, "r") as input_file:
-            # Read the data from the FASTA file
-            data = input_file.read()
-
-            # Encrypt the data using the AES cipher
-            encrypted_data = cipher.encrypt(data)
-
-            # Write the encrypted data to the SecureFASTA file
-            f.write(encrypted_data)
+        # Write the encrypted data to the SecureFASTA file
+        f.write(encrypted_data)
 
 def decrypt_secure_fasta(input_file, output_file, password):
     """Decrypts a SecureFASTA file and writes the result to a FASTA file."""
     # Open the input file for reading
-    with open(input_file, "r") as f:
+    with open(input_file, "rb") as f:
         # Read the salt, iterations, and IV from the file
-        salt = f.read(8)
-        iterations = f.read(4)
+        salt = f.read(16)
+        iterations = int.from_bytes(f.read(8),'big')
         iv = f.read(16)
 
         # Use PBKDF2 to derive a key from the password
-        key = PBKDF2(password, salt, dkLen=32, count=iterations)
+        key = PBKDF2(password, salt, dkLen=32, count=iterations, hmac_hash_module=SHA256)
 
         # Initialize the AES cipher in cipher block chaining (CBC) mode
         cipher = AES.new(key, AES.MODE_CBC, iv)
@@ -77,10 +80,10 @@ def decrypt_secure_fasta(input_file, output_file, password):
         encrypted_data = f.read()
 
         # Decrypt the data using the AES cipher
-        data = cipher.decrypt(encrypted_data)
+        data = unpad(cipher.decrypt(encrypted_data),16)
 
         # Write the decrypted data to the output file
-        with open(output_file, "w") as output_file:
+        with open(output_file, "wb") as output_file:
             output_file.write(data)
 
 def generate_checksum(input_file):
@@ -90,10 +93,8 @@ def generate_checksum(input_file):
         # Read the data from the file
         data = f.read()
 
-        # Calculate the SHA-256 hash of the data
-        checksum = hashlib.sha256(data).hexdigest()
-
-        return checksum
+    # Calculate the SHA-256 hash of the data
+    return SHA256.new(data).digest()
 
 def parse_args():
     # Create an ArgumentParser object
@@ -107,6 +108,7 @@ def parse_args():
 
     # Add an argument for the public key file (for encryption) or the private key file (for decryption)
     parser.add_argument("--public-key-file", required=False, help="the file containing the public key for encryption")
+    parser.add_argument("--private-key-file", required=False, help="the file containing the private key for decryption")
     parser.add_argument("--key-file", required=False, help="the file containing the private key for decryption")
 
     # Add an argument to specify whether to encrypt or decrypt the file
@@ -118,7 +120,6 @@ def parse_args():
     print(args)
     return args
 
-
 def main():
     # Parse the command-line arguments
     args = parse_args()
@@ -128,37 +129,37 @@ def main():
         password = generate_random_password()
 
         # Load the public key for the recipient
-        with open(args.public_key_file, "r") as f:
-            public_key_data = f.read()
-        public_key = ECC.import_key(public_key_data)
+        with open(args.public_key_file, "rb") as f:
+            public_key = RSA.importKey(f.read())
 
         # Encrypt the password using the public key
-        encrypted_password = encrypt_password(password, public_key)
+        encrypted_password = encrypt_with_rsa(str.encode(password), public_key)
 
         # Encrypt the FASTA file
         encrypt_secure_fasta(args.input_file, args.output_file, password)
 
         # Generate a checksum for the SecureFASTA file
         checksum = generate_checksum(args.output_file)
+        encrypted_checksum = encrypt_with_rsa(checksum,public_key)
 
         # Write the encrypted password and checksum to a separate file
-        with open(args.key_file, "w") as f:
+        with open(args.key_file, "wb") as f:
             f.write(encrypted_password)
-            f.write(checksum)
+            f.write(encrypted_checksum)
 
     elif args.decrypt:
         # Load the private key for the recipient
-        with open(args.private_key_file, "r") as f:
-            private_key_data = f.read()
-        private_key = ECC.import_key(private_key_data)
+        with open(args.private_key_file, "rb") as f:
+            private_key = RSA.importKey(f.read())
 
         # Load the encrypted password and checksum from the key file
-        with open(args.key_file, "r") as f:
-            encrypted_password = f.read(64)
-            checksum = f.read(64)
+        with open(args.key_file, "rb") as f:
+            encrypted_password = f.read(256)
+            encrypted_checksum = f.read(256)
 
         # Decrypt the password using the private key
-        password = private_key.decrypt(encrypted_password)
+        password = decrypt_with_rsa(encrypted_password, private_key)
+        checksum = decrypt_with_rsa(encrypted_checksum, private_key)
 
         # Validate the checksum
         if generate_checksum(args.input_file) != checksum:
